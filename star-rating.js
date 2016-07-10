cedf411134903e9bf4b62c78329eea0d6554bf0e
Ben Groulx <bgroulx@mobify.com>
2016-07-10 12:06:47 -0700

    💪🏼 Star Rating component

---
+++ star-rating.js
@@ -0,0 +1,6 @@
+<div class="c-star-ratings" data-rating="{rating}" data-rating-max="{ratingMax}" aria-label="Rated {rating} stars out of {ratingMax}">
+
+    <span class="c-star-ratings__item {?ratingFilled}c--filled{/ratingFilled}"></span>
+    <span class="c-star-ratings__value">{rating}/{ratingMax}</span>
+
+</div>
